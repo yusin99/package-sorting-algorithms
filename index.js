ed131c68a390b3ee1893592f964322ac3229525d
Yusin-Kiril Sedrati <ysedrati@netisse.fr>
2022-05-20 19:10:00 +0200

    selectionSort algorithm + package version bump 1.0.2

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
+function swap(arr, a, b) {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
+
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1
+};
+
+function defaultCompare(a, b) {
+  if (a === b) {
+      return 0;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
 // Bubble sort Implementation using Javascript
   
   
-// Creating the bblSort function
- function bblSort(arr){
-     
- for(var i = 0; i < arr.length; i++){
-     
-   // Last i elements are already in place  
-   for(var j = 0; j < ( arr.length - i -1 ); j++){
-       
-     // Checking if the item at present iteration 
-     // is greater than the next iteration
-     if(arr[j] > arr[j+1]){
-         
-       // If the condition is true then swap them
-       var temp = arr[j]
-       arr[j] = arr[j + 1]
-       arr[j+1] = temp
-     }
-   }
- }
- // Print the sorted array
- console.log(arr);
+function bubbleSort(arr, compare = defaultCompare) {
+  const { length } = arr;
+  for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - 1 - i; j++) { // refer to note below
+          if (compare(arr[j], arr[j + 1]) === Compare.BIGGER_THAN) {
+              swap(arr, j, j + 1);
+          }
+      }
+  }
+  console.log(arr);
 }
 
-module.exports.bblSort = bblSort;
+// function selectionSort(arr, compare = defaultCompare) {
+//     const { length } = arr;
+//     let minIndex;
+//     for (let i = 0; i < length - 1; i++) {
+//         minIndex = i;
+//         for (let j = i; j < length; j++) {
+//             if (compare(arr[minIndex], arr[j]) === Compare.BIGGER_THAN) {
+//                 minIndex = j;
+//             }
+//         }
+//         if (i !== minIndex) {
+//             swap(arr, i, minIndex);
+//         }
+//     }
+//     console.log(arr);
+//   }
+
+
+module.exports = {bubbleSort,selectionSort};
