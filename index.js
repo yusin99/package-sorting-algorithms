ba3b01c85f3d0e38401a41860828561c9aa0b954
Yusin-Kiril Sedrati <ysedrati@netisse.fr>
2022-05-20 19:11:25 +0200

    .

--- index.js
+++ index.js
@@ -31,22 +31,22 @@ function bubbleSort(arr, compare = defaultCompare) {
   console.log(arr);
 }
 
-// function selectionSort(arr, compare = defaultCompare) {
-//     const { length } = arr;
-//     let minIndex;
-//     for (let i = 0; i < length - 1; i++) {
-//         minIndex = i;
-//         for (let j = i; j < length; j++) {
-//             if (compare(arr[minIndex], arr[j]) === Compare.BIGGER_THAN) {
-//                 minIndex = j;
-//             }
-//         }
-//         if (i !== minIndex) {
-//             swap(arr, i, minIndex);
-//         }
-//     }
-//     console.log(arr);
-//   }
+function selectionSort(arr, compare = defaultCompare) {
+    const { length } = arr;
+    let minIndex;
+    for (let i = 0; i < length - 1; i++) {
+        minIndex = i;
+        for (let j = i; j < length; j++) {
+            if (compare(arr[minIndex], arr[j]) === Compare.BIGGER_THAN) {
+                minIndex = j;
+            }
+        }
+        if (i !== minIndex) {
+            swap(arr, i, minIndex);
+        }
+    }
+    console.log(arr);
+  }
 
 
 module.exports = {bubbleSort,selectionSort};
