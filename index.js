62c831a9c824d8a763381cca05384a0f2d381180
Yusin-Kiril Sedrati <ysedrati@netisse.fr>
2022-05-21 11:21:50 +0200

    Insertion sort algorithm

--- index.js
+++ index.js
@@ -45,4 +45,19 @@ function swap(arr, a, b) {
       console.log(arr);
     }
   
-  module.exports = {bubbleSort,selectionSort};
+    function insertionSort(arr, compare = defaultCompare) {
+      const { length } = arr;
+      let temp;
+      for (let i = 1; i < length; i++) {
+          let j = i;
+          temp = arr[i];
+          while (j > 0 && compare(arr[j - 1], temp) === Compare.BIGGER_THAN) {
+              arr[j] = arr[j - 1];
+              j--;
+          }
+          arr[j] = temp;
+      }
+      return arr;
+  }
+  
+  module.exports = {bubbleSort,selectionSort,insertionSort};
